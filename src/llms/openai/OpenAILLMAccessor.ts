ea53065c1b580106394220f06dc90563092b265a
mcgear <michael.gearhardt@gmail.com>
2023-09-27 11:53:09 -0600

    Chat without stream

--- src/llms/openai/OpenAILLMAccessor.ts
+++ src/llms/openai/OpenAILLMAccessor.ts
@@ -1,11 +1,11 @@
-import { ConversationMessage } from "../../conversations/ConversationMessage.ts";
-import { Personality } from "../../personalities/Personality.ts";
+import { ConversationMessage } from '../../conversations/ConversationMessage.ts';
+import { Personality } from '../../personalities/Personality.ts';
 import {
   AzureExtensionsOptions,
   convertAsyncIterable,
   OpenAIClient,
-} from "../../src.deps.ts";
-import { ILLMAccessor, LLMAccessorOptions } from "../ILLMAccessor.ts";
+} from '../../src.deps.ts';
+import { ILLMAccessor, LLMAccessorOptions } from '../ILLMAccessor.ts';
 
 export type OpenAILLMAccessorOptions = {
   Extensions?: AzureExtensionsOptions;
@@ -14,15 +14,49 @@ export type OpenAILLMAccessorOptions = {
 } & LLMAccessorOptions;
 
 export class OpenAILLMAccessor
-  implements ILLMAccessor<OpenAILLMAccessorOptions> {
+  implements ILLMAccessor<OpenAILLMAccessorOptions>
+{
   constructor(protected openAiClient: OpenAIClient) {}
+
+  public async Chat(
+    personality: Personality,
+    messages: ConversationMessage[],
+    options: OpenAILLMAccessorOptions = {
+      Model: 'gpt-35-turbo',
+    }
+  ): Promise<string | null | undefined> {
+    const chatMessages = messages.map((msg) => {
+      return {
+        role: msg.From,
+        content: msg.Content,
+      };
+    });
+
+    const chatCompletions = await this.openAiClient.getChatCompletions(
+      options?.Model!,
+      [
+        {
+          role: 'system',
+          content: `${personality.Declarations} ${personality.Instructions}`,
+        },
+        ...chatMessages,
+      ],
+      {
+        // azureExtensionOptions: options?.Extensions,
+        maxTokens: personality.MaxTokens,
+        temperature: personality.Temperature,
+      }
+    );
+
+    return chatCompletions.choices[0]?.message?.content;
+  }
 
   public async ChatStream(
     personality: Personality,
     messages: ConversationMessage[],
     options: OpenAILLMAccessorOptions = {
-      Model: "gpt-35-turbo",
-    },
+      Model: 'gpt-35-turbo',
+    }
   ): Promise<AsyncIterable<string | null | undefined>> {
     const chatMessages = messages.map((msg) => {
       return {
@@ -35,7 +69,7 @@ export class OpenAILLMAccessor
       options?.Model!,
       [
         {
-          role: "system",
+          role: 'system',
           content: `${personality.Declarations} ${personality.Instructions}`,
         },
         ...chatMessages,
@@ -45,9 +79,7 @@ export class OpenAILLMAccessor
         maxTokens: personality.MaxTokens,
         temperature: personality.Temperature,
         stream: options?.Stream,
-        // functionCall: reports ? { name: "GlenReport" } : null,
-        // functions: reports,
-      },
+      }
     );
 
     const iterable = convertAsyncIterable(chatCompletions, (event) => {
--- tests/llms/openai/OpenAILLMAccessor.tests.ts
+++ tests/llms/openai/OpenAILLMAccessor.tests.ts
@@ -1,35 +1,35 @@
-import { OpenAILLMAccessor } from "../../../src/llms/openai/OpenAILLMAccessor.ts";
-import { Personality } from "../../../src/personalities/Personality.ts";
-import { AzureKeyCredential, OpenAIClient } from "../../../src/src.deps.ts";
-import { assertFalse } from "../../tests.deps.ts";
+import { OpenAILLMAccessor } from '../../../src/llms/openai/OpenAILLMAccessor.ts';
+import { Personality } from '../../../src/personalities/Personality.ts';
+import { AzureKeyCredential, OpenAIClient } from '../../../src/src.deps.ts';
+import { assertFalse } from '../../tests.deps.ts';
 
-Deno.test("OpenAILLMAccessor tests", async (t) => {
-  const endpoint = "https://thinky-ai-prd.openai.azure.com"; //Deno.env.get('OPENAI_ENDPOINT') || '';
-  const azureApiKey = "10d6457a8ee34b1db592be343c5209d3"; // Deno.env.get('OPENAI_API_KEY') || '';
+Deno.test('OpenAILLMAccessor tests', async (t) => {
+  const endpoint = 'https://thinky-ai-prd.openai.azure.com'; //Deno.env.get('OPENAI_ENDPOINT') || '';
+  const azureApiKey = '10d6457a8ee34b1db592be343c5209d3'; // Deno.env.get('OPENAI_API_KEY') || '';
 
   const client = new OpenAIClient(
     endpoint,
-    new AzureKeyCredential(azureApiKey),
+    new AzureKeyCredential(azureApiKey)
   );
 
   const llm = new OpenAILLMAccessor(client);
 
   const personality: Personality = {
-    Declarations: ["You are a super helpful AI assistant."],
+    Declarations: ['You are a super helpful AI assistant.'],
   };
 
-  await t.step("ChatStream tests", async () => {
+  await t.step('ChatStream tests', async () => {
     const chatParts = await llm.ChatStream(personality, [
       {
-        From: "assistant",
+        From: 'assistant',
         Content:
-          "Please provide me a story that starts `Once upon a time` and is only 1 paragraph.",
+          'Please provide me a story that starts `Once upon a time` and is only 1 paragraph.',
       },
     ]);
 
     let i = 0;
 
-    let full = "";
+    let full = '';
     for await (const chatPart of chatParts) {
       i++;
 
@@ -39,6 +39,18 @@ Deno.test("OpenAILLMAccessor tests", async (t) => {
     }
 
     assertFalse(i == 1);
-    assertFalse(!full.startsWith("Once upon a time"));
+    assertFalse(!full.startsWith('Once upon a time'));
+  });
+
+  await t.step('Chat tests', async () => {
+    const fullChat = await llm.Chat(personality, [
+      {
+        From: 'assistant',
+        Content:
+          'Please provide me a story that starts `Once upon a time` and is only 1 paragraph.',
+      },
+    ]);
+
+    assertFalse(!fullChat!.startsWith('Once upon a time'));
   });
 });
