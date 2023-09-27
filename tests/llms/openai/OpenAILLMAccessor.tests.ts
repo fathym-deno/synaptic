b3a1e19209d4c6e11f1e0e9b02890028efccd4a3
mcgear <michael.gearhardt@gmail.com>
2023-09-27 15:05:48 -0600

    Hide keys

--- tests/llms/openai/OpenAILLMAccessor.tests.ts
+++ tests/llms/openai/OpenAILLMAccessor.tests.ts
@@ -1,40 +1,40 @@
-import { OpenAILLMAccessor } from "../../../src/llms/openai/OpenAILLMAccessor.ts";
-import { Personality } from "../../../src/personalities/Personality.ts";
+import { OpenAILLMAccessor } from '../../../src/llms/openai/OpenAILLMAccessor.ts';
+import { Personality } from '../../../src/personalities/Personality.ts';
 import {
   AzureKeyCredential,
   FunctionDefinition,
   OpenAIClient,
-} from "../../../src/src.deps.ts";
-import { assertExists, assertFalse } from "../../tests.deps.ts";
-import { FunctionToCall } from "../../../src/llms/ILLMAccessor.ts";
+} from '../../../src/src.deps.ts';
+import { assertExists, assertFalse } from '../../tests.deps.ts';
+import { FunctionToCall } from '../../../src/llms/ILLMAccessor.ts';
 
-Deno.test("OpenAILLMAccessor tests", async (t) => {
-  const endpoint = "https://thinky-ai-prd-east-us-code.openai.azure.com"; //Deno.env.get('OPENAI_ENDPOINT') || '';
-  const azureApiKey = "236b38e41e9d41fca60680c6910d953e"; // Deno.env.get('OPENAI_API_KEY') || '';
+Deno.test('OpenAILLMAccessor tests', async (t) => {
+  const endpoint = Deno.env.get('OPENAI_ENDPOINT') || '';
+  const azureApiKey = Deno.env.get('OPENAI_API_KEY') || '';
 
   const client = new OpenAIClient(
     endpoint,
-    new AzureKeyCredential(azureApiKey),
+    new AzureKeyCredential(azureApiKey)
   );
 
   const llm = new OpenAILLMAccessor(client);
 
   const personality: Personality = {
-    Declarations: ["You are a super helpful AI assistant."],
+    Declarations: ['You are a super helpful AI assistant.'],
   };
 
-  await t.step("ChatStream tests", async () => {
+  await t.step('ChatStream tests', async () => {
     const chatParts = await llm.ChatStream(personality, [
       {
-        From: "assistant",
+        From: 'assistant',
         Content:
-          "Please provide me a story that starts `Once upon a time` and is only 1 paragraph.",
+          'Please provide me a story that starts `Once upon a time` and is only 1 paragraph.',
       },
     ]);
 
     let i = 0;
 
-    let full = "";
+    let full = '';
     for await (const chatPart of chatParts) {
       i++;
 
@@ -44,34 +44,34 @@ Deno.test("OpenAILLMAccessor tests", async (t) => {
     }
 
     assertFalse(i == 1);
-    assertFalse(!full.startsWith("Once upon a time"));
+    assertFalse(!full.startsWith('Once upon a time'));
   });
 
-  await t.step("Chat tests", async () => {
+  await t.step('Chat tests', async () => {
     const fullChat = (await llm.Chat(personality, [
       {
-        From: "assistant",
+        From: 'assistant',
         Content:
-          "Please provide me a story that starts `Once upon a time` and is only 1 paragraph.",
+          'Please provide me a story that starts `Once upon a time` and is only 1 paragraph.',
       },
     ])) as string;
 
-    assertFalse(!fullChat!.startsWith("Once upon a time"));
+    assertFalse(!fullChat!.startsWith('Once upon a time'));
   });
 
-  await t.step("Function Call Chat tests", async () => {
+  await t.step('Function Call Chat tests', async () => {
     const funcToCall = (await llm.Chat(
       personality,
       [
         {
-          From: "user",
-          Content: "Please provide with a report about the state of IIoT.",
+          From: 'user',
+          Content: 'Please provide with a report about the state of IIoT.',
         },
       ],
       {
-        Model: "gpt-35-turbo-16k",
+        Model: 'gpt-35-turbo-16k',
         Functions: [loadTestFunction()],
-      },
+      }
     )) as FunctionToCall;
 
     assertExists(funcToCall);
@@ -82,22 +82,22 @@ Deno.test("OpenAILLMAccessor tests", async (t) => {
 
 export function loadTestFunction(): FunctionDefinition {
   return {
-    name: "Test",
-    description: "This is a test",
+    name: 'Test',
+    description: 'This is a test',
     parameters: {
-      type: "object",
+      type: 'object',
       properties: {
         reportTitle: {
-          type: "string",
-          description: "The title of the report.",
+          type: 'string',
+          description: 'The title of the report.',
         },
         reportSubhead: {
-          type: "string",
+          type: 'string',
           description:
-            "A more detailed description of the report for us in describing what can be found.",
+            'A more detailed description of the report for us in describing what can be found.',
         },
       },
-      required: ["reportTitle", "reportSubhead"],
+      required: ['reportTitle', 'reportSubhead'],
     },
   };
 }
