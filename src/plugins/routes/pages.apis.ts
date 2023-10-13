b6267ec614e521fccd7d2eca7f331e129090055a
mcgear <michael.gearhardt@gmail.com>
2023-10-13 09:30:51 -0600

    Updates to api

--- src/plugins/routes/pages.apis.ts
+++ src/plugins/routes/pages.apis.ts
@@ -50,6 +50,7 @@ export function pagesRoute(
     async POST(req, _ctx) {
       const page: Page = await req.json();
 
+      console.log(page);
       await pages.Save(page);
 
       return new Response(JSON.stringify({ Status: "Success" }), {
