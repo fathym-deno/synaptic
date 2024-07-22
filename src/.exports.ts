e021e1a5ec1a8170f252bb770693c70dae23ede3
mcgear <michael.gearhardt@gmail.com>
2024-07-22 13:39:56 -0600

    new utils

---
+++ src/.exports.ts
@@ -0,0 +1,7 @@
+export * from './eac/_exports.ts';
+export * from './indexing/_exports.ts';
+export * from './memory/_exports.ts';
+export * from './plugins/_exports.ts';
+export * from './runnables/_exports.ts';
+export * from './testing/.exports.ts';
+export * from './utils/.exports.ts';
---
+++ src/utils/messages.ts
@@ -0,0 +1,43 @@
+import {
+  AIMessage,
+  AIMessageChunk,
+  BaseMessage,
+  HumanMessage,
+  HumanMessageChunk,
+} from '../src.deps.ts';
+
+export function lastAiNotHumanMessages(messages?: BaseMessage[]) {
+  return lastMessagesOfType(
+    messages ?? [],
+    [AIMessage.name, AIMessageChunk.name],
+    [HumanMessage.name, HumanMessageChunk.name]
+  );
+}
+
+export function lastMessagesOfType(
+  messages: BaseMessage[],
+  types: string[],
+  endTypes?: string[]
+) {
+  let hitEnd = false;
+
+  const lastMsgs = messages?.reverse().reduce((acc, msg) => {
+    if (!hitEnd) {
+      if (types.includes(msg.constructor.name)) {
+        acc.unshift(msg);
+      }
+
+      if (
+        endTypes
+          ? endTypes.includes(msg.constructor.name)
+          : !types.includes(msg.constructor.name)
+      ) {
+        hitEnd = true;
+      }
+    }
+
+    return acc;
+  }, [] as BaseMessage[]);
+
+  return lastMsgs;
+}
