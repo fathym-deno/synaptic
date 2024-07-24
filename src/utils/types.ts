d1222c530fd3b1ecd11bbfc789bbc9ff92633db7
mcgear <michael.gearhardt@gmail.com>
2024-07-24 16:45:17 -0600

    Support arrays in TypeToZod

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -11,6 +11,6 @@ export type TypeToZod<T> = {
   [K in keyof T]: T[K] extends string | number | boolean | null | undefined
     ? undefined extends T[K]
       ? z.ZodOptional<z.ZodType<Exclude<T[K], undefined>>>
-    : z.ZodType<T[K]>
-    : z.ZodObject<TypeToZod<T[K]>>;
+      : z.ZodType<T[K]>
+    : z.ZodObject<TypeToZod<T[K]>> | z.ZodType<T[K]>;
 };
